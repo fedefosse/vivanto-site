4e0837ea67a611ab9081cc511944820640eae202
fedefosse <fedefosse1@gmail.com>
2025-10-14 10:37:55 -0500

    fix: disable image optimization for Vercel static load

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // No detengas el build por errores de ESLint en Vercel
-    ignoreDuringBuilds: true,
+  eslint: { ignoreDuringBuilds: true },
+  images: {
+    unoptimized: true, // ← esto debe estar
   },
-  // Si Vercel volviera a fallar por tipos TS, descomenta la línea siguiente:
-  // typescript: { ignoreBuildErrors: true },
 };
 
 module.exports = nextConfig;
